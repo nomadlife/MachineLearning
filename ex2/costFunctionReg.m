20c85d85df7f14371e7cf145591cd149df345bbf
jw <joonwoo3816@gmail.com>
2017-06-13 10:59:53 +0900

    5L

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -20,9 +20,9 @@ grad = zeros(size(theta));
 
 z=zeros(rows(X));
 h=0;
-w=0;
-r=0;
-t=0;
+%w=0;
+%r=0;
+%t=0;
 
 for i=1:m
 	z(i)=X(i,:)*theta;
@@ -30,21 +30,35 @@ for i=1:m
 
 	J=J+((-y(i))*log(h)-(1-y(i))*log(1-h))/m;
 
-	w=w+(h-y(i))*X(i,1)/m;
-	r=r+(h-y(i))*X(i,2)/m;
-	t=t+(h-y(i))*X(i,3)/m;
+	%w=w+(h-y(i))*X(i,1)/m;
+	%r=r+(h-y(i))*X(i,2)/m;
+	%t=t+(h-y(i))*X(i,3)/m;
+	
 end
 
 for j=1:size(theta,1)
-	J=J+(theta(j)^2)*lambda/(2*m)
+	if (j>=2)
+	J=J+(theta(j)^2)*lambda/(2*m);
+	end	
+end
+
+for j=1:size(theta,1)
+
+	for i=1:m
+		z(i)=X(i,:)*theta;
+		h=1./(1+e.^-z(i));
+		grad(j)=grad(j)+(h-y(i))*X(i,j)/m;
+	end
 	
+	if (j>=2)
+		grad(j)=grad(j)+theta(j)*lambda/m;
+	end
 	
 end
 
 
-J
-grad=[w,r,t]
-
+%J
+%grad=[w,r,t]
 
 
 % =============================================================
