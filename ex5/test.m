2a2949c089e76c5ff5610d04eb03dd27df9c3eaf
jw <joonwoo3816@gmail.com>
2017-06-26 09:22:28 +0900

    14L_Ex5

---
+++ ex5/test.m
@@ -0,0 +1,46 @@
+%     linearRegCostFunction.m
+%     learningCurve.m
+%     validationCurve.m
+
+%% Initialization
+clear ; close all; clc
+
+%% =========== Part 1: Loading and Visualizing Data =============
+%  We start the exercise by first loading and visualizing the dataset. 
+%  The following code will load the dataset into your environment and plot
+%  the data.
+%
+
+% Load Training Data
+fprintf('Loading and Visualizing Data ...\n')
+
+% Load from ex5data1: 
+% You will have X, y, Xval, yval, Xtest, ytest in your environment
+load ('ex5data1.mat');
+
+% m = Number of examples
+m = size(X, 1);
+
+% Plot training data
+%plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+xlabel('Change in water level (x)');
+ylabel('Water flowing out of the dam (y)');
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%% =========== Part 2: Regularized Linear Regression Cost =============
+%  You should now implement the cost function for regularized linear 
+%  regression. 
+%
+
+theta = [1 ; 1];
+J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+
+fprintf(['Cost at theta = [1 ; 1]: %f '...
+         '\n(this value should be about 303.993192)\n'], J);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+
