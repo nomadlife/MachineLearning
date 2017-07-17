fa1b85ee72688b02cd08534f7f95329fe8674722
nomadlife <joonwoo3816@gmail.com>
2017-07-17 22:59:23 +0900

    23L3

---
+++ ex6/ex6_spam_test.m
@@ -0,0 +1,33 @@
+clear ; close all; clc
+
+%% ==================== Part 1: Email Preprocessing ====================
+
+fprintf('\n===Part1 Preprocessing sample email (emailSample1.txt)\n');
+
+% Extract Features
+file_contents = readFile('emailSample1.txt');
+word_indices  = processEmail(file_contents);
+
+% Print Stats
+fprintf('Word Indices: \n');
+fprintf(' %d', word_indices);
+fprintf('\n\n');
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%% ==================== Part 2: Feature Extraction ====================
+
+fprintf('\n===Part2 Extracting features from sample email (emailSample1.txt)\n');
+
+% Extract Features
+file_contents = readFile('emailSample1.txt');
+word_indices  = processEmail(file_contents);
+features      = emailFeatures(word_indices);
+
+% Print Stats
+fprintf('Length of feature vector: %d\n', length(features));
+fprintf('Number of non-zero entries: %d\n', sum(features > 0));
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
