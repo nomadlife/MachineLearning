62b31b2affadd237bcd5ef9c6eed5ff8b44621f9
nomadlife <joonwoo3816@gmail.com>
2017-07-24 19:28:37 +0900

    28L4

--- ex7/ex7_test2.m
+++ ex7/ex7_test2.m
@@ -62,3 +62,37 @@ initial_centroids = kMeansInitCentroids(X, K);
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
+
+%% ================= Part 5: Image Compression ======================
+%  In this part of the exercise, you will use the clusters of K-Means to
+%  compress an image. To do this, we first find the closest clusters for
+%  each example. After that, we
+
+fprintf('\nApplying K-Means to compress an image.\n\n');
+
+% Find closest cluster members
+idx = findClosestCentroids(X, centroids);
+
+% Essentially, now we have represented the image X as in terms of the
+% indices in idx.
+
+% We can now recover the image from the indices (idx) by mapping each pixel
+% (specified by its index in idx) to the centroid value
+X_recovered = centroids(idx,:);
+
+% Reshape the recovered image into proper dimensions
+X_recovered = reshape(X_recovered, img_size(1), img_size(2), 3);
+
+% Display the original image
+subplot(1, 2, 1);
+imagesc(A);
+title('Original');
+
+% Display compressed image side by side
+subplot(1, 2, 2);
+imagesc(X_recovered)
+title(sprintf('Compressed, with %d colors.', K));
+
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
